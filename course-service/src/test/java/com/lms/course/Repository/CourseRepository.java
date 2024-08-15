417ed8710f481b522f9f7e9d5d10900adbfe613c
janithachamaka <wdjchamaka@students.nsbm.ac.lk>
2024-08-15 22:43:37 +0530

    Revert "Course Microservice added"
    This reverts commit b79fd5ee2e5704506ef1980533fe2d79c2d3bf2f.

--- course-service/src/test/java/com/lms/course/Repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lms.course.Repository;
-
-import com.lms.course.Model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-}
